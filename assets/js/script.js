684c3f9af8f7d45b91857041157dea63d219c315
Ahmet Sami Gül <a_sami_gul@Ahmet-MacBook-Air-4.local>
2025-02-27 12:40:18 +0300

    hamburger menu eklendi, mobil responsive eklendi

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ function changeImage(smlimage) {
     let smlimages = document.querySelectorAll(".smlimage");
     smlimages.forEach(img => img.classList.remove("active"));
     smlimage.classList.add("active");
+
 }
 
     let count = 0;
@@ -12,14 +13,24 @@ function changeImage(smlimage) {
     function increaseCount() {
         count++; 
         document.getElementById("counter").textContent = count;
-    }
+    };
 
     function decreaseCount() {
         if (count > 0) { 
             count--;
             document.getElementById("counter").textContent = count;
         }
-    }
+    };
     function addtoCart() {
         alert(`${count} items have been added to yourt shopcart`);
-    }
+    };
+
+    const menu_btn = document.querySelector('.hamburger-menu');
+    const mobile_nav = document.querySelector('.mobile-nav');
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+        mobile_nav.classList.toggle('is-active');
+    });
+
+    
+    
